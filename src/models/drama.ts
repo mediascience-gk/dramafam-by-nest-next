79b6ca293bdf94b4f33e24fb14b5e00a5d57b1c1
mediasciencejp <contact@mediascience.jp>
2022-02-27 11:43:10 +0900

    add convertKanaToKanaStatus class

--- src/models/drama.ts
+++ src/models/drama.ts
@@ -1,8 +1,11 @@
+import { ConvertKanaToKanaStatus } from './convertKanaToKanaStatus';
+
 export class Drama {
   private name: string;
   private season: string;
   private permalink: string;
   private kana: string;
+  private kanaStatus: string;
   private tvCompany: string;
   private startAt: string;
   private endAt: string;
@@ -25,8 +28,12 @@ export class Drama {
     this.tvCompany = props.tvCompany;
     this.startAt = props.startAt;
     this.endAt = props.endAt;
+
+    const convertKanaToKanaStatus = new ConvertKanaToKanaStatus(props.kana);
+    this.kanaStatus = convertKanaToKanaStatus.convert();
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   save() {}
 
   private validateProps(props: {
@@ -43,5 +50,36 @@ export class Drama {
     } else if (props.name?.length < 1) {
       throw new Error('zero-length name');
     }
+
+    if (props.permalink === null) {
+      throw new Error('null permalink');
+    } else if (props.permalink?.length < 1) {
+      throw new Error('zero-length permalink');
+    }
+
+    if (props.kana === null) {
+      throw new Error('null kana');
+    } else if (props.kana?.length < 1) {
+      throw new Error('zero-length kana');
+    } else if (!this.isKana(props.kana)) {
+      throw new Error('not-kana kana');
+    }
+
+    if (props.tvCompany === null) {
+      throw new Error('null tvCompany');
+    } else if (props.tvCompany?.length < 1) {
+      throw new Error('zero-length tvCompany');
+    }
+
+    const kanaCon = new ConvertKanaToKanaStatus(props.kana);
+    console.log(kanaCon.convert());
   }
+
+  isKana = function (kana: string) {
+    if (kana.match(/^[ぁ-んー]*$/) || kana.match(/^[ァ-ヶー]+$/)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 }
