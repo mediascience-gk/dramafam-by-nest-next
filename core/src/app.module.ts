7a34e45b10c8851dac7d17cad82b05d546b2e16d
sundaycrafts <sundaycrafts@users.noreply.github.com>
2022-04-03 17:32:33 +0200

    refactor(core/orm): commonize orm config

--- core/src/app.module.ts
+++ core/src/app.module.ts
@@ -1,30 +1,13 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 
 import { EntryModule } from './modules/entry.module';
 import { CommentModule } from './modules/comment.module';
 import { DramaModule } from './modules/drama.module';
-import { CommentModel } from './orm/comment.model';
-import { DramaModel } from './orm/drama.model';
-import { EntryModel } from './orm/entry.model';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'test',
-      password: 'passwd',
-      database: 'sample',
-      entities: [CommentModel, DramaModel, EntryModel],
-      synchronize: true,
-    }),
-    CommentModule,
-    EntryModule,
-    DramaModule,
-  ],
+  imports: [CommentModule, EntryModule, DramaModule, CommonModule],
   controllers: [],
   providers: [],
 })
--- core/test/entry.e2e-spec.ts
+++ core/test/entry.e2e-spec.ts
@@ -2,27 +2,13 @@ import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { EntryModule } from '../src/modules/entry.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { CommentModel } from '../src/orm/comment.model';
-import { EntryModel } from '../src/orm/entry.model';
+import { CommonModule } from '../src/common/common.module';
 
 let app: INestApplication;
 
 beforeEach(async () => {
   const moduleFixture = await Test.createTestingModule({
-    imports: [
-      TypeOrmModule.forRoot({
-        type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'test',
-        password: 'passwd',
-        database: 'sample',
-        entities: [CommentModel, EntryModel],
-        synchronize: true,
-      }),
-      EntryModule,
-    ],
+    imports: [CommonModule, EntryModule],
   }).compile();
 
   app = moduleFixture.createNestApplication();
