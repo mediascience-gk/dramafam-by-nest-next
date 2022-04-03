51118de9f7ff7227847d9b9ecf445de04c469957
sundaycrafts <sundaycrafts@users.noreply.github.com>
2022-04-03 17:56:23 +0200

    test(core/entry): wrap spec with describe

--- core/test/entry.e2e-spec.ts
+++ core/test/entry.e2e-spec.ts
@@ -4,17 +4,19 @@ import { INestApplication } from '@nestjs/common';
 import { EntryModule } from '../src/modules/entry.module';
 import { CommonModule } from '../src/common/common.module';
 
-let app: INestApplication;
+describe(EntryModule, () => {
+  let app: INestApplication;
 
-beforeEach(async () => {
-  const moduleFixture = await Test.createTestingModule({
-    imports: [CommonModule, EntryModule],
-  }).compile();
+  beforeEach(async () => {
+    const moduleFixture = await Test.createTestingModule({
+      imports: [CommonModule, EntryModule],
+    }).compile();
 
-  app = moduleFixture.createNestApplication();
-  await app.init();
-});
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
 
-it(`/GET entry`, () => {
-  return request(app.getHttpServer()).get('/entry').expect(200);
+  it(`/GET entry`, () => {
+    return request(app.getHttpServer()).get('/entry').expect(200);
+  });
 });
