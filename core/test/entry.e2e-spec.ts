1ca7129bb997afd33cc64f38befd7be73459ec6a
sundaycrafts <sundaycrafts@users.noreply.github.com>
2022-04-03 17:27:41 +0200

    fix(core/orm): fix failed e2e test deps

--- core/test/entry.e2e-spec.ts
+++ core/test/entry.e2e-spec.ts
@@ -2,12 +2,27 @@ import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { EntryModule } from '../src/modules/entry.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommentModel } from '../src/orm/comment.model';
+import { EntryModel } from '../src/orm/entry.model';
 
 let app: INestApplication;
 
 beforeEach(async () => {
   const moduleFixture = await Test.createTestingModule({
-    imports: [EntryModule],
+    imports: [
+      TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'test',
+        password: 'passwd',
+        database: 'sample',
+        entities: [CommentModel, EntryModel],
+        synchronize: true,
+      }),
+      EntryModule,
+    ],
   }).compile();
 
   app = moduleFixture.createNestApplication();
