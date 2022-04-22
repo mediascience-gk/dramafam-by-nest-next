5b8b6ac4ef7303f76f884255eaa2b37dddc0db61
mediasciencejp <contact@mediascience.jp>
2022-04-23 08:25:54 +0900

    CRUD in drama.service.spec

--- core/src/services/drama.service.spec.ts
+++ core/src/services/drama.service.spec.ts
@@ -2,26 +2,90 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { DramaService } from './drama.service';
 import { StaticDramaRepository } from '../interface-adapter/gateways/drama/drama.repository';
 import { DramaRepository } from '../models/drama/drama.repository';
-
-const mockDramaRepository = () => ({});
+import { Drama } from '../models/drama/drama';
+import { CreateDramaDto } from './dto/create-drama.dto';
 
 describe('DramaService', () => {
   let service: DramaService;
-  let dramaRepository: DramaRepository;
+  let stubDramaRepository: Partial<DramaRepository>;
+  let mockCreateDramaDto: CreateDramaDto;
 
   beforeEach(async () => {
+    mockCreateDramaDto = {
+      title: 'DramaTitle',
+      permalink: 'drama-title',
+      kana: 'ドラマタイトル',
+      startAt: '2022-04-01',
+      endAt: null,
+    };
+
+    stubDramaRepository = {
+      findById: (id: number) => {
+        return Promise.resolve({
+          id,
+          title: 'DramaTitle',
+          permalink: 'drama-title',
+          kana: 'ドラマタイトル',
+        } as Drama);
+      },
+      create: (creteDramaDto: CreateDramaDto) => {
+        return Promise.resolve({
+          id: 1,
+          title: 'DramaTitle',
+          permalink: 'drama-title',
+          kana: 'ドラマタイトル',
+        } as Drama);
+      },
+      findAll: () => {
+        return Promise.resolve([
+          {
+            id: 1,
+            title: 'DramaTitle',
+            permalink: 'drama-title',
+            kana: 'ドラマタイトル',
+          } as Drama,
+          {
+            id: 2,
+            title: 'DramaTitle',
+            permalink: 'drama-title',
+            kana: 'ドラマタイトル',
+          } as Drama,
+        ]);
+      },
+      delete: (id) => {
+        return Promise.resolve();
+      },
+    };
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         DramaService,
-        { provide: StaticDramaRepository, useFactory: mockDramaRepository },
+        { provide: StaticDramaRepository, useValue: stubDramaRepository },
       ],
     }).compile();
 
     service = module.get<DramaService>(DramaService);
-    dramaRepository = module.get<DramaRepository>(StaticDramaRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('findById', async () => {
+    const drama = await service.findById(1);
+    expect(drama.id).toBe(1);
+  });
+
+  it('create', async () => {
+    const drama = await service.create(mockCreateDramaDto);
+    expect(drama.id).toBe(1);
+  });
+
+  it('findAll', async () => {
+    const dramas = await service.findAll();
+    expect(dramas.length).toBe(2);
+  });
+
+  it('remove', async () => {
+    expect(service.remove(1)).toBeDefined();
   });
 });
