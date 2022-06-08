bcffaeb8d41b966faa8765b6324bf04418ba265b
mediasciencejp <contact@mediascience.jp>
2022-06-09 07:12:54 +0900

    feat(web): common layout (responsive)

--- web/components/templates/CommonLayout.tsx
+++ web/components/templates/CommonLayout.tsx
@@ -1,4 +1,5 @@
 import { FC, ReactNode } from 'react';
+import { MediaQuery, MantineProvider, Container, Grid } from '@mantine/core';
 import Head from 'next/head';
 
 type Props = {
@@ -11,13 +12,22 @@ export const CommonLayout: FC<Props> = ({
   title = 'ドラマふぁむ',
 }) => {
   return (
-    <div className="flex min-h-screen">
+    <MantineProvider>
       <Head>
         <title>{title}</title>
       </Head>
-      <header></header>
-      <main className="flex flex-1 flex-col p-4">{children}</main>
-      <footer></footer>
-    </div>
+      <Container size="xl" p={20}>
+        <Grid gutter="xs">
+          <Grid.Col md={8} lg={9}>
+            {children}
+          </Grid.Col>
+          <MediaQuery smallerThan="md" styles={{ display: 'none' }}>
+            <Grid.Col md={4} lg={3} className="bg-slate-100">
+              サイドバー
+            </Grid.Col>
+          </MediaQuery>
+        </Grid>
+      </Container>
+    </MantineProvider>
   );
 };
